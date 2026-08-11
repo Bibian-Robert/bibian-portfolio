8de01af91fdc0a602957ba21f0721808d7b0595b
Bibian-Robert <bibiannjiru44@gmail.com>
2026-08-11 22:01:23 +0300

    Revert Projects grid to 3 columns for wider cards

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -118,7 +118,7 @@ export default function Projects({ activeSkill, onClearSkill }) {
           </div>
         )}
 
-        <div className="grid grid-cols-1 gap-7 sm:grid-cols-2 lg:grid-cols-4">
+        <div className="grid grid-cols-1 gap-7 sm:grid-cols-2 lg:grid-cols-3">
           {projects.map((project) => (
             <ProjectCard key={project.title} project={project} activeSkill={activeSkill} />
           ))}
